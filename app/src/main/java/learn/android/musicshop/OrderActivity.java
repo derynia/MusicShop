2d7f24ddcf6ca99f446b5262014175c3a60189a7
Alexey Shamarin <derynia@gmail.com>
2020-05-24 01:04:14 +0300

    Added send by e-mail and styles for other activity. Here the app is ready to test.

--- app/src/main/java/learn/android/musicshop/OrderActivity.java
+++ app/src/main/java/learn/android/musicshop/OrderActivity.java
@@ -3,10 +3,14 @@ package learn.android.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
+    private Order order;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,30 @@ public class OrderActivity extends AppCompatActivity {
 
         TextView orderTextView  = findViewById(R.id.orderTextView);
         Bundle arguments = getIntent().getExtras();
-        final Order order;
         if (arguments != null) {
             order = (Order) arguments.getSerializable(Order.class.getSimpleName());
             orderTextView.setText(order.toStringForTextView());
+        }
+    }
+
+    public void submitOrder(View view) {
+        if (order == null) {
+            Toast.makeText(getApplicationContext(), "Order does not exist", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String[] addresses = {"derynia@gmail.com"};
+        String subject = "Order from Music Shop";
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        //intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, order.toStringForTextView());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
         }
     }
 }
